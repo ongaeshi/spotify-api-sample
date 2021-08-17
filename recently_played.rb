72839d38a87c8c2c1c897dc14116cdc6c1c8868b
ongaeshi <ongaeshi0621@gmail.com>
2021-08-17 23:46:49 +0900

    feat: embedded bootstrap

--- recently_played.rb
+++ recently_played.rb
@@ -58,11 +58,24 @@ end
 
 MyApplication.run! if __FILE__ == $0
 
-
 __END__
 
 @@ layout
+<!doctype html>
 <html>
-  <h1>Spotify recently played</h1>
-  <%= yield %>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Spotify recently played</title>
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <main class="container">
+      <div class="starter-template text-left py-5 px-3">
+        <h1>Spotify recently played</h1>
+        <%= yield %>
+      </div>
+    </main>
+  </body>
 </html>
